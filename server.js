0c1a5ef016508ed3863ec658c4814053aa405a93
manasa-277 <73336679+manasa-277@users.noreply.github.com>
2024-01-02 19:53:36 +0530

    get user details

--- server.js
+++ server.js
@@ -4,6 +4,15 @@ const app = express();
 app.get('/api/get',(req,res)=>{
     res.send({message:"welcome to motus"})
 })
+app.get('/api/get_user_details',(req,res) => {
+    res.send({
+        user:{
+            name:"manasa",
+            age:22,
+            contact:12254
+        },env:process.env.NAME
+    })
+});
 
 app.listen(3001, () => {
     console.log("listening on port 3001");
